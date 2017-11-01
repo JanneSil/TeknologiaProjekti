6cb1f293f1453deb1915b27d51ba23ac0cb9a11a
JanneS <jannesillanpaa@kamk.fi>
2017-11-01 23:47:01 +0200

    Scripts

---
+++ TeknoProjekti/Assets/PlayerMovementScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovementScript : MonoBehaviour
+{
+
+    public float speed;
+    private float moveVertical;
+    private float moveHorizontal;
+
+
+    // Use this for initialization
+    void Start()
+    {
+ 
+    }
+
+    void Update()
+    {
+        moveVertical = Input.GetAxis("Vertical");
+        moveHorizontal = Input.GetAxis("Horizontal");
+    }
+    void FixedUpdate()
+    {
+
+        if (Input.GetButton("Vertical"))
+                transform.Translate(0, 1 * moveVertical * speed * Time.deltaTime, 0);
+        if (Input.GetButton("Horizontal"))
+                transform.Translate(1 * moveHorizontal * speed * Time.deltaTime, 0, 0);
+
+    }
+}
