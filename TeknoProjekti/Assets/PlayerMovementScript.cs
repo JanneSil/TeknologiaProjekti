be03aaa2b3088699bba208ac3b6f97005730f17f
JanneS <jannesillanpaa@kamk.fi>
2017-11-05 16:14:20 +0200

    Melee & Range attack / map

--- TeknoProjekti/Assets/PlayerMovementScript.cs
+++ TeknoProjekti/Assets/PlayerMovementScript.cs
@@ -5,14 +5,21 @@ public class PlayerMovementScript : MonoBehaviour
 {
 
     public float speed;
+    public GameObject playerRangePos;
+    public GameObject playerArrow;
     private float moveVertical;
     private float moveHorizontal;
+    GameObject enemy;
+    private bool inRange;
+
+    public float timeBetweenAttacks = 0.5f;
+    float timer;
 
 
     // Use this for initialization
     void Start()
     {
- 
+
     }
 
     void Update()
@@ -24,9 +31,45 @@ public class PlayerMovementScript : MonoBehaviour
     {
 
         if (Input.GetButton("Vertical"))
-                transform.Translate(0, 1 * moveVertical * speed * Time.deltaTime, 0);
+            transform.Translate(0, 1 * moveVertical * speed * Time.deltaTime, 0);
         if (Input.GetButton("Horizontal"))
-                transform.Translate(1 * moveHorizontal * speed * Time.deltaTime, 0, 0);
+            transform.Translate(1 * moveHorizontal * speed * Time.deltaTime, 0, 0);
 
+        timer += Time.deltaTime;
+
+        if (Input.GetButton("Fire1"))
+        {
+            //Play Animation
+            //Play Sound
+            if (inRange)
+            {
+                // Do damage;
+            }
+        }
+
+        if (Input.GetButton("Fire2") && timer >= timeBetweenAttacks)
+        {
+            //Animation
+            //Sound
+            Instantiate(playerArrow, playerRangePos.transform.position, playerRangePos.transform.rotation);
+            timer = 0f;
+        }
+
+    }
+
+    void OnTriggerEnter (Collider other)
+    {
+        if (other.gameObject == enemy)
+        {
+            inRange = true;
+        }
+    }
+
+    void OnTriggerExit (Collider other)
+    {
+        if (other.gameObject == enemy)
+        {
+            inRange = false;
+        }
     }
 }
---
+++ TeknoProjekti/Assets/Scripts/PlayerArrowScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerArrowScript : MonoBehaviour
+{
+
+    public float speed;
+
+    public Rigidbody2D rb;
+
+    public float MaxLifeTime = 3f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        Destroy(gameObject, MaxLifeTime);
+
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = transform.right * speed;
+    }
+}
